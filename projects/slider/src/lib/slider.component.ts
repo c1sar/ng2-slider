6fe0ba8fdcf2fdf31c2cd51deab3448e6f44da88
César R. Contreras <c1sar90@gmail.com>
2018-08-12 00:46:37 -0500

    fix slider mouseout event

--- projects/slider/src/lib/slider.component.ts
+++ projects/slider/src/lib/slider.component.ts
@@ -25,6 +25,7 @@ export class SliderComponent implements OnInit, AfterViewInit {
   lastScrollLeft: number = 0;
   slideNumber: number = 4;
   currentSlidePos: number = 1;
+  blocked: boolean = false;
 
   constructor() { }
 
@@ -37,9 +38,13 @@ export class SliderComponent implements OnInit, AfterViewInit {
   }
 
   setSlide(numberSlide: number) {
+    console.log('set slider');
     const width = this.sliderElement.clientWidth;
     this.sliderElement.scrollLeft = width * (numberSlide - 1);
     this.currentSlidePos = numberSlide;
+    setTimeout(() => {
+      this.blocked = false;
+    }, 200);
   }
 
   clickEvent(slide: ISlide) {
@@ -47,15 +52,22 @@ export class SliderComponent implements OnInit, AfterViewInit {
   }
 
   @HostListener('mousedown', ['$event']) onMouseDown(e: MouseEvent) {
+    document.getSelection().empty();
+    if (this.validateElementBullet(e)) {
+      e.preventDefault();
+      this.blocked = true;
+      return;
+    }
+    console.log('mousedown');
     this.isDragging = true;
     this.posSlider.posInitX = e.clientX;
   }
 
   @HostListener('mousemove', ['$event']) mouseMove(e: MouseEvent) {
-
-    if (!this.isDragging) {
+    if ((!this.isDragging) || (this.blocked)) {
       return;
     }
+    console.log('mousemove');
 
     const width = this.sliderElement.scrollWidth - this.sliderElement.clientWidth;
     const newScrollLeftPosition = this.sliderElement.scrollLeft - e.movementX;
@@ -66,11 +78,23 @@ export class SliderComponent implements OnInit, AfterViewInit {
   }
 
   @HostListener('mouseout', ['$event']) onMouseOut(e: MouseEvent) {
+    console.log('mouseout');
+    console.log(e);
+    if ((this.blocked) || (this.validateElementBullet(e)) || (e.relatedTarget == null)
+    || ((e.relatedTarget as HTMLElement).className === 'bullet-container')
+    ) {
+      console.log('rechazado');
+      return;
+    }
+    console.log('permitido');
     this.isDragging = false;
     this.setSlide(this.currentSlidePos);
   }
 
   @HostListener('mouseup', ['$event']) onMouseUp(e: MouseEvent) {
+    if (this.blocked) {
+      return;
+    }
     this.isDragging = false;
     this.posSlider.posEndX = e.clientX;
     this.move();
@@ -78,14 +102,18 @@ export class SliderComponent implements OnInit, AfterViewInit {
 
 
   @HostListener('touchstart', ['$event']) onTouchStart(e: TouchEvent) {
+    console.log('touchstart');
+    if (this.blocked) {
+      return;
+    }
     this.isDragging = true;
     this.posSlider.posInitX = e.touches[0].clientX;
     this.posSlider.scrollInit = this.sliderElement.scrollLeft;
   }
 
   @HostListener('touchmove', ['$event']) onTouchMove(e: TouchEvent) {
-
-    if (!this.isDragging) {
+    console.log('touchmove');
+    if ((!this.isDragging) || (this.blocked)) {
       return;
     }
 
@@ -100,6 +128,10 @@ export class SliderComponent implements OnInit, AfterViewInit {
   }
 
   @HostListener('touchend', ['$event']) onTouchEnd(e: TouchEvent) {
+    console.log('touchend');
+    if (this.blocked) {
+      return;
+    }
     this.isDragging = false;
     this.move();
   }
@@ -134,11 +166,30 @@ export class SliderComponent implements OnInit, AfterViewInit {
       position = this.easeOutSine(t, posInit, c, 380);
       this.sliderElement.scrollLeft = position;
       t = t + 5;
-    }, 5)
+    }, 5);
   }
+
+  // setSlider(event: Event, slideInit: number, slideEnd: number): void {
+  //   this.isDragging = true;
+  //   const posEnd = this.sliderElement.clientWidth * (slideEnd - 1);
+  //   this.sliderElement.scrollLeft = posEnd;
+  //   this.currentSlidePos = slideEnd;
+  //   this.isDragging = false;
+  // }
 
   easeOutSine(t, b, c, d) {
     return c * Math.sin(t / d * (Math.PI / 2)) + b;
+  }
+
+  private validateElementBullet(e: Event): boolean {
+    if (e.target) {
+      const element = e.target as HTMLElement;
+      if ((element.className === 'bullet') || (element.className === 'bullet-container')
+      || (element.className === 'bullet active')) {
+        return true;
+      }
+    }
+    return false;
   }
 
 }
