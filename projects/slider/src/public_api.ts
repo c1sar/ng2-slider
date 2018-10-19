8c48aed8ceed3fd2d2fbd325fda7d7e52b475ca7
César R. Contreras <c1sar90@gmail.com>
2018-10-18 20:23:17 -0500

    fix models exports

--- projects/slider/src/public_api.ts
+++ projects/slider/src/public_api.ts
@@ -2,6 +2,12 @@
  * Public API Surface of slider
  */
 
-export * from './lib/slider.service';
+export * from './lib/models/ISliderEvent';
+export * from './lib/models/IOptions';
+export * from './lib/models/ISlide';
+export * from './lib/models/IButton';
+export * from './lib/models/animation-type.enum';
+export * from './lib/models/bullet-type.enum';
+export * from './lib/models/IButton';
 export * from './lib/slider.component';
 export * from './lib/slider.module';
